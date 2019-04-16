51247cdf6a95d4a3721bc1bf7baa9c0a591ce296
Joshua Elder <jcelder88@gmail.com>
2019-04-16 15:16:25 -0700

    Add image model

--- pics/lib/src/models/image_model.dart
+++ pics/lib/src/models/image_model.dart
@@ -1,2 +1,18 @@
+class ImageModel {
+  int id;
+  int albumId;
+  String title;
+  String url;
+  String thumbnailUrl;
 
+  ImageModel(this.id, this.albumId, this.title, this.url, this.thumbnailUrl);
+
+  ImageModel.fromJson(Map<String, dynamic> parsedJson) {
+    id = parsedJson['id'];
+    albumId = parsedJson['albumId'];
+    title = parsedJson['title'];
+    url = parsedJson['url'];
+    thumbnailUrl = parsedJson['thumbnailUrl'];
+  }
+}
 
