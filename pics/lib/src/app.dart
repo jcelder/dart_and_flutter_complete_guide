92131972110de66255dba3114f4d7dc4b080cc28
Joshua Elder <jcelder88@gmail.com>
2019-04-16 14:23:05 -0700

    Refactor to stateful widget

--- pics/lib/src/app.dart
+++ pics/lib/src/app.dart
@@ -1,15 +1,33 @@
 import 'package:flutter/material.dart';
 
-class App extends StatelessWidget {
+class App extends StatefulWidget {
+  @override
+  _AppState createState() => _AppState();
+}
+
+class _AppState extends State<App> {
+  int _imagesShown = 0;
+
+  void incrementImagesShown() {
+    setState(() {
+      _imagesShown += 1;
+    });
+  }
+
+  @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
           title: Text('Lets see some images'),
         ),
+        body: Text(
+          '$_imagesShown Images',
+          textAlign: TextAlign.center,
+          ),
         floatingActionButton: FloatingActionButton(
           onPressed: () {
-            print('Hi there!');
+            incrementImagesShown();
           },
           child: Icon(Icons.add),
         ),
