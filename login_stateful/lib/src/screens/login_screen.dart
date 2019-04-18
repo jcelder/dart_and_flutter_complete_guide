a5f1766f198b951c8c60c47c63c97aa2ba040c65
Joshua Elder <jcelder88@gmail.com>
2019-04-18 11:33:21 -0700

    Implement form validation and saving

--- login_stateful/lib/src/screens/login_screen.dart
+++ login_stateful/lib/src/screens/login_screen.dart
@@ -5,29 +5,73 @@ class LoginScreen extends StatefulWidget{
 }
 
 class _LoginScreenState extends State<LoginScreen> {
+  final _formKey = GlobalKey<FormState>();
+  String email = '';
+  String password = '';
+
   Widget build(BuildContext build) {
     return Container(
       margin: EdgeInsets.all(20.0),
       child: Form(
+        key: _formKey,
         child: Column(
           children: <Widget>[
-            TextFormField(
-              decoration: const InputDecoration(
-                labelText: 'Email'
-              ),
-            ),
-            TextFormField(
-              decoration: const InputDecoration(
-                labelText: 'Password'
-              ),
-            ),
-            RaisedButton(
-              onPressed: () {},
-              child: Text('Submit'),
-            ),
+            emailField(),
+            passwordField(),
+            Container(margin: EdgeInsets.only(top: 25.0)),
+            submitButton(),
           ],
         )
       )
+    );
+  }
+
+  Widget emailField() {
+    return TextFormField(
+      decoration: const InputDecoration(
+        labelText: 'Email Address',
+        hintText: 'you@example.com'
+      ),
+      keyboardType: TextInputType.emailAddress,
+      validator: (String value) {
+        bool isValid = value.contains(new RegExp('.+@.+'));
+        // return null if valid
+        // otherwise string with the error message if invalid
+        return isValid ? null : 'Invalid Email Address';
+      },
+      onSaved: (String value) {
+        email = value;
+      },
+    );
+  }
+
+  Widget passwordField() {
+    return TextFormField(
+      decoration: const InputDecoration(
+        labelText: 'Password',
+        hintText: 'Password',
+      ),
+      obscureText: true,
+      validator: (String value) {
+        bool isValid = value.length >= 8;
+        return isValid ? null : 'Invalid Password';
+      },
+      onSaved: (String value) {
+        password = value;
+      }
+    );
+  }
+
+  Widget submitButton() {
+    return RaisedButton(
+      child: Text('Submit'),
+      color: Colors.blue,
+      onPressed: () {
+        if (_formKey.currentState.validate()) {
+          _formKey.currentState.save();
+        }
+        print('Email: $email, Password: $password');
+      },
     );
   }
 }
